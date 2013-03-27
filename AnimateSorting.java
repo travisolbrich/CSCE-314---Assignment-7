c06a76b201b71ac43a47572c90edabf422ca75e4
Travis Olbrich <travis.olbrich@outlook.com>
2013-03-27 14:47:48 -0500

    When the individual threads are created, individual unique frames are created. These will hold the animations.

--- AnimateSorting.java
+++ AnimateSorting.java
@@ -3,21 +3,26 @@
  * 
  */  
 
+ 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Vector;
+
 /**
  * AnimateSorting is the master class that controls the individual sorting threads.
  * After taking the user's vector size preference, control is handed to the individual
  * threads which handle the rest of the program.
  *
  * @author  Travis Olbrich
- * 
- * @param   args    the first integer in args is the preferred vector size
  */
-public class AnimateSorting {
+public class AnimateSorting extends JFrame{
     public static void main(String args[]) {
-        new SortThread("bubble");
-        new SortThread("quick");
+        Vector toSort = new Vector();
+
+        SortThread bubble = new SortThread("bubble", toSort);
+        SortThread quick = new SortThread("quick", toSort);
         
-        System.out.println("[DEBUG] Main thread exiting");
     }
 
 
@@ -27,32 +32,41 @@ public class AnimateSorting {
      * algorithms are called from here.
      *
      * @author  Travis Olbrich
-     *
-     * @param   algorithm   the name of the algorithm we would like to run
-     * @param   items       the vector of integer numbers to sort
      */
     static class SortThread implements Runnable {
         Thread t;
+        String algorithm;
+        Vector toSort;
 
-        SortThread(String algorithm) {
-            t = new Thread(this, "Sorting Thread (" + algorithm + ")");
-            System.out.println("[DEBUG] Child thread: " + t);
+        SortThread(String a, Vector s) {
+            algorithm = a;
+            toSort = s;
+
+            t = new Thread(this, algorithm);
             t.start(); 
+            
         }
        
         public void run() {
             try {
-                for(int i = 5; i > 0; i--) {
-                    System.out.println("[DEBUG] Child Thread (" + t + "): " + i);
-                
-                    Thread.sleep(500);
-                }
-            } catch (InterruptedException e) {
-                System.out.println("[DEBUG] Thread interrupted.");
+                JFrame frame = new JFrame(algorithm);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+         
+                JLabel emptyLabel = new JLabel(t.toString());
+                emptyLabel.setPreferredSize(new Dimension(175, 100));
+                frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+         
+                //Display the window.
+                frame.pack();
+                frame.setLocationByPlatform(true);
+                frame.setVisible(true);
+
+            } catch (Exception e) {
             }
-            System.out.println("[DEBUG] Exiting child thread (" + t + ").");
         }
     }
+
+    
 }
 
 
