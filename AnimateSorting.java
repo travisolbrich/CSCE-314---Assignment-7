cff97b00fbb4be970d4b31b7b79bb5c0f69bb419
Travis Olbrich <travis.olbrich@outlook.com>
2013-03-27 13:51:19 -0500

    Demonstrating that a single SortThread class allows unique threads to be built from it. Improved commenting.

--- AnimateSorting.java
+++ AnimateSorting.java
@@ -1,39 +1,55 @@
+/*
+ * CSCE 314 Assignment 7
+ * 
+ */  
+
+/**
+ * AnimateSorting is the master class that controls the individual sorting threads.
+ * After taking the user's vector size preference, control is handed to the individual
+ * threads which handle the rest of the program.
+ *
+ * @author  Travis Olbrich
+ * 
+ * @param   args    the first integer in args is the preferred vector size
+ */
 public class AnimateSorting {
-   public static void main(String args[]) {
-      new NewThread(); // create a new thread
-      try {
-         for(int i = 5; i > 0; i--) {
-           System.out.println("Main Thread: " + i);
-           Thread.sleep(1000);
-         }
-      } catch (InterruptedException e) {
-         System.out.println("Main thread interrupted.");
-      }
-      System.out.println("Main thread exiting.");
-   }
+    public static void main(String args[]) {
+        new SortThread("bubble");
+        new SortThread("quick");
+        
+        System.out.println("Main thread exiting");
+    }
 }
 
-// Create a new thread.
-class NewThread implements Runnable {
-   Thread t;
-   NewThread() {
-      // Create a new, second thread
-      t = new Thread(this, "Demo Thread");
-      System.out.println("Child thread: " + t);
-      t.start(); // Start the thread
-   }
+/**
+ * Each SortThread is the generic structure that handles each individual sorting 
+ * algorithm. Each individual graphical window is created here and the sorting
+ * algorithms are called from here.
+ *
+ * @author  Travis Olbrich
+ *
+ * @param   algorithm   the name of the algorithm we would like to run
+ * @param   items       the vector of integer numbers to sort
+ */
+class SortThread implements Runnable {
+    Thread t;
+
+    SortThread(String algorithm) {
+        t = new Thread(this, "Sorting Thread (" + algorithm + ")");
+        System.out.println("Child thread: " + t);
+        t.start(); 
+    }
    
-   // This is the entry point for the second thread.
-   public void run() {
-      try {
-         for(int i = 5; i > 0; i--) {
-            System.out.println("Child Thread: " + i);
-            // Let the thread sleep for a while.
-            Thread.sleep(500);
-         }
-     } catch (InterruptedException e) {
-         System.out.println("Child interrupted.");
-     }
-     System.out.println("Exiting child thread.");
-   }
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Child Thread (" + t + "): " + i);
+            
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted.");
+        }
+        System.out.println("Exiting child thread (" + t + ").");
+    }
 }
